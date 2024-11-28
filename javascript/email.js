78db9f2415c2f9aa4f3fe4ad36c1c49593bc085f
Alexander Moravski <alexmoravski@gmail.com>
2024-11-28 20:28:37 +0200

    Add files via upload

---
+++ javascript/email.js
@@ -0,0 +1,9 @@
+function email(host,domain1,domain2,domain3) {
+	a_href = '<a href='+'m'+'a'+'i'+'l'+'t'+'o'+':'+host+'&#64'+domain1+'.'+domain2+'.'+domain3+'>'+host+'&#64'+domain1+'.'+domain2+'.'+domain3+'</a>';
+	document.write(a_href);
+}
+
+function contact(name,host,domain1,domain2,domain3) {
+	a_href = '<a href='+'m'+'a'+'i'+'l'+'t'+'o'+':'+host+'&#64'+domain1+'.'+domain2+'.'+domain3+'>'+name+'</a>';
+	document.write(a_href);
+}
