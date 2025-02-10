deab7177e51baee15b94f723512aae00c2a9128d
Alexander Moravski <alexmoravski@gmail.com>
2025-02-10 17:12:06 +0200

    Ver 2

--- javascript/email.js
+++
@@ -1,9 +0,0 @@
-function email(host,domain1,domain2,domain3) {
-	a_href = '<a href='+'m'+'a'+'i'+'l'+'t'+'o'+':'+host+'&#64'+domain1+'.'+domain2+'.'+domain3+'>'+host+'&#64'+domain1+'.'+domain2+'.'+domain3+'</a>';
-	document.write(a_href);
-}
-
-function contact(name,host,domain1,domain2,domain3) {
-	a_href = '<a href='+'m'+'a'+'i'+'l'+'t'+'o'+':'+host+'&#64'+domain1+'.'+domain2+'.'+domain3+'>'+name+'</a>';
-	document.write(a_href);
-}
